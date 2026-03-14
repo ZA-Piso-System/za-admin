f6c072b1e975d2e0710fe5dd699b1299060b240e
jasonlerit <jasonlerit077@gmail.com>
2026-03-14 23:02:09 +0800

    feat: update client UI

--- app/(protected)/clients/[id]/page.tsx
+++ app/(protected)/clients/[id]/page.tsx
@@ -9,6 +9,13 @@ import { useQuery } from "@tanstack/react-query"
 import { useParams } from "next/navigation"
 import { useEffect, useMemo, useState } from "react"
 import { format } from "date-fns"
+import {
+  Item,
+  ItemContent,
+  ItemDescription,
+  ItemGroup,
+  ItemTitle,
+} from "@/components/ui/item"
 
 export default function ViewClient() {
   const { id } = useParams<{ id: string }>()
@@ -53,25 +60,55 @@ export default function ViewClient() {
   const { startAt, endAt } = useMemo(() => {
     const startAt = data?.startAt
       ? format(new Date(data.startAt), "MMM dd, yyyy, hh:mm:ss a")
-      : ""
+      : "-"
 
     const endAt = data?.endAt
       ? format(new Date(data.endAt), "MMM dd, yyyy, hh:mm:ss a")
-      : ""
+      : "-"
 
     return { startAt, endAt }
   }, [data])
 
   return (
     <div className="flex flex-col items-center justify-center gap-4 p-4">
-      <div>
-        <div>Device ID: {data?.deviceId}</div>
-        <div>PC No: {data?.pcNo}</div>
-        <div>Status: {data?.status}</div>
-        <div>Remaining Time: {secondsToHMS()}</div>
-        <div>Start At: {startAt}</div>
-        <div>End At: {endAt}</div>
-      </div>
+      <ItemGroup className="grid grid-cols-1 sm:grid-cols-2">
+        <Item variant="outline">
+          <ItemContent>
+            <ItemTitle>Device ID</ItemTitle>
+            <ItemDescription>{data?.deviceId}</ItemDescription>
+          </ItemContent>
+        </Item>
+        <Item variant="outline">
+          <ItemContent>
+            <ItemTitle>PC No</ItemTitle>
+            <ItemDescription>{data?.pcNo}</ItemDescription>
+          </ItemContent>
+        </Item>
+        <Item variant="outline">
+          <ItemContent>
+            <ItemTitle>Status</ItemTitle>
+            <ItemDescription>{data?.status}</ItemDescription>
+          </ItemContent>
+        </Item>
+        <Item variant="outline">
+          <ItemContent>
+            <ItemTitle>Current Time</ItemTitle>
+            <ItemDescription>{secondsToHMS()}</ItemDescription>
+          </ItemContent>
+        </Item>
+        <Item variant="outline">
+          <ItemContent>
+            <ItemTitle>Start At</ItemTitle>
+            <ItemDescription>{startAt}</ItemDescription>
+          </ItemContent>
+        </Item>
+        <Item variant="outline">
+          <ItemContent>
+            <ItemTitle>End At</ItemTitle>
+            <ItemDescription>{endAt}</ItemDescription>
+          </ItemContent>
+        </Item>
+      </ItemGroup>
       <div className="flex w-full flex-col gap-2">
         <AddTimeDialog />
         <Button>Update Time</Button>
