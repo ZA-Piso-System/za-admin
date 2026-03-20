16eff2c5a37b40294bc999d7fab52b4bd8efc66b
jasonlerit <jasonlerit077@gmail.com>
2026-03-20 21:46:01 +0800

    fix: guest layout

--- app/(guest)/layout.tsx
+++ app/(guest)/layout.tsx
@@ -1,23 +1,9 @@
 "use client"
 
-import { authClient } from "@/lib/auth-client"
-import { useRouter } from "next/navigation"
-import { useEffect } from "react"
-
 export default function GuestLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const router = useRouter()
-  const { data, isPending } = authClient.useSession()
-
-  useEffect(() => {
-    if (data === null && !isPending) {
-      router.push("/login")
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [data, isPending])
-
   return <main className="w-full p-4">{children}</main>
 }
