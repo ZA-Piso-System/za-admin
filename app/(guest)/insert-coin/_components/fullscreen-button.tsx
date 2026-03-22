844a0b06aae5415f88b696baf0e5245d75a29bf0
jasonlerit <jasonlerit077@gmail.com>
2026-03-22 20:40:19 +0800

    fix: show maximize button only

--- app/(guest)/insert-coin/_components/fullscreen-button.tsx
+++ app/(guest)/insert-coin/_components/fullscreen-button.tsx
@@ -1,25 +1,20 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
-import { MaximizeIcon, MinimizeIcon } from "lucide-react"
+import { MaximizeIcon } from "lucide-react"
 import { useState } from "react"
 
 export const FullscreenButton = () => {
   const [isFullscreen, setIsFullscreen] = useState<boolean>(false)
 
   const toggleFullscreen = () => {
-    if (isFullscreen) {
-      document.exitFullscreen()
-      setIsFullscreen(false)
-    } else {
-      document.documentElement.requestFullscreen()
-      setIsFullscreen(true)
-    }
+    setIsFullscreen(true)
+    document.documentElement.requestFullscreen()
   }
 
-  return (
+  return !isFullscreen ? (
     <Button size="icon" onClick={toggleFullscreen}>
-      {isFullscreen ? <MinimizeIcon /> : <MaximizeIcon />}
+      <MaximizeIcon />
     </Button>
-  )
+  ) : null
 }
