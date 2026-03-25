c007bb93a21fca054215272ca34fac3d948f17e7
jasonlerit <jasonlerit077@gmail.com>
2026-03-26 07:47:18 +0800

    fix: build issue

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,11 +1,14 @@
 import { RecentTransactions } from "@/app/(protected)/dashboard/_components/recent-transactions"
 import { SectionCards } from "@/app/(protected)/dashboard/_components/section-cards"
+import { Suspense } from "react"
 
 export default function Dashboard() {
   return (
-    <div className="@container/main flex flex-col gap-6">
-      <SectionCards />
-      <RecentTransactions />
-    </div>
+    <Suspense>
+      <div className="@container/main flex flex-col gap-6">
+        <SectionCards />
+        <RecentTransactions />
+      </div>
+    </Suspense>
   )
 }
