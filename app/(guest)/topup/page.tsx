354eab8fe1e8d1a2116159471cb17731b1dfa6d8
jasonlerit <jasonlerit077@gmail.com>
2026-04-08 22:21:56 +0800

    fix: build issue

--- app/(guest)/topup/page.tsx
+++ app/(guest)/topup/page.tsx
@@ -3,23 +3,26 @@ import { CurrentDate } from "@/components/shared/current-date"
 import { Button } from "@/components/ui/button"
 import { ChevronLeftIcon } from "lucide-react"
 import Link from "next/link"
+import { Suspense } from "react"
 
 export default function TopUp() {
   return (
-    <div className="flex flex-col gap-8 p-4">
-      <div className="flex items-center justify-between">
-        <Button className="w-fit" variant="outline" size="lg" asChild>
-          <Link href="/">
-            <ChevronLeftIcon />
-            Back
-          </Link>
-        </Button>
-        <h1 className="text-3xl font-bold tracking-widest uppercase">
-          Select User
-        </h1>
-        <CurrentDate />
+    <Suspense>
+      <div className="flex flex-col gap-8 p-4">
+        <div className="flex items-center justify-between">
+          <Button className="w-fit" variant="outline" size="lg" asChild>
+            <Link href="/">
+              <ChevronLeftIcon />
+              Back
+            </Link>
+          </Button>
+          <h1 className="text-3xl font-bold tracking-widest uppercase">
+            Select User
+          </h1>
+          <CurrentDate />
+        </div>
+        <UserList />
       </div>
-      <UserList />
-    </div>
+    </Suspense>
   )
 }
