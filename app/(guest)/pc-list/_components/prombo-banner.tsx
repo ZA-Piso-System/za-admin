a190e4ea58cb5cdac1054645b8ce3535d6401453
jasonlerit <jasonlerit077@gmail.com>
2026-04-10 11:57:14 +0800

    fix: promo banner

--- app/(guest)/pc-list/_components/prombo-banner.tsx
+++ app/(guest)/pc-list/_components/prombo-banner.tsx
@@ -3,7 +3,7 @@ import Marquee from "react-fast-marquee"
 export const PromoBanner = () => {
   return (
     <Marquee className="text-3xl font-bold">
-      💰 Top up ka na! May play time ka na, may points ka pa pang dagdag hours!
+      💰 Top up na! May play time ka na, may points ka pa pang dagdag hours!
     </Marquee>
   )
 }
