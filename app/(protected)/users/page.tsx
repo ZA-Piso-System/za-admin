ffcea5ce403145a63eacbdda1f9dec664fee4f3c
jasonlerit <jasonlerit077@gmail.com>
2026-04-13 11:34:51 +0800

    fix: build issue

--- app/(protected)/users/page.tsx
+++ app/(protected)/users/page.tsx
@@ -1,15 +1,18 @@
 import { UserList } from "@/app/(protected)/users/_components/user-list"
+import { Suspense } from "react"
 
 export default function Users() {
   return (
-    <div className="flex flex-col gap-6 p-4">
-      <div className="flex items-end justify-between">
-        <div className="flex flex-col">
-          <h1 className="text-2xl font-semibold tracking-tight">Users</h1>
-          <p className="text-muted-foreground">Manage users</p>
+    <Suspense>
+      <div className="flex flex-col gap-6 p-4">
+        <div className="flex items-end justify-between">
+          <div className="flex flex-col">
+            <h1 className="text-2xl font-semibold tracking-tight">Users</h1>
+            <p className="text-muted-foreground">Manage users</p>
+          </div>
         </div>
+        <UserList />
       </div>
-      <UserList />
-    </div>
+    </Suspense>
   )
 }
