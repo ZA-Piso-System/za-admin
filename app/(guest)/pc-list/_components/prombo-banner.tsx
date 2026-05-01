75b9dda1b32f3acd28035a61ee3d92644a8795cd
jasonlerit <jasonlerit077@gmail.com>
2026-05-01 16:27:13 +0800

    chore: update promo banner

--- app/(guest)/pc-list/_components/prombo-banner.tsx
+++ app/(guest)/pc-list/_components/prombo-banner.tsx
@@ -3,7 +3,7 @@ import Marquee from "react-fast-marquee"
 export const PromoBanner = () => {
   return (
     <Marquee className="flex gap-8 text-3xl font-bold">
-      🔥 Top up na! ₱5 = 20 mins + 5 points • 🎁 75 points = FREE 1 hour 🚀
+      🔥 Top up na! ₱5 = 20 mins + 5 points • 🎁 100 points = FREE 1 hour 🚀
     </Marquee>
   )
 }
